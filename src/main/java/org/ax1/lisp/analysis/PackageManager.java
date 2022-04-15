088ac27a8a38b17bdb4dabd2b45e44d3941371c7
JeromeA <jerome.abela@gmail.com>
2022-04-15 11:06:34 +0100

    Add CommonLispPackage to hold CL symbols.

--- src/main/java/org/ax1/lisp/analysis/PackageManager.java
+++ src/main/java/org/ax1/lisp/analysis/PackageManager.java
@@ -3,20 +3,12 @@ package org.ax1.lisp.analysis;
 import com.intellij.openapi.components.Service;
 import com.intellij.openapi.project.Project;
 
-import java.util.Collection;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Set;
 
 @Service(Service.Level.PROJECT)
 public final class PackageManager {
-  private static final Collection<String> COMMON_LISP_FUNCTIONS = Set.of(
-      "+", "-", "<", "<=", ">", ">=", "adjust-array", "and", "ash", "assoc", "cdr", "char-code",
-      "characterp", "declare", "elt", "eq", "equal", "error", "eval", "format", "funcall", "if",
-      "gethash", "ignore", "incf", "integerp", "length", "list", "make-hash-table", "member",
-      "not", "null", "return", "second", "setf", "setq", "special", "symbol-value", "symbolp",
-      "unless", "when");
-  private static final Collection<String> COMMON_LISP_VARIABLES = Set.of("nil", "t");
 
   private final Map<String, Package> packages = new HashMap<>();
 
@@ -25,9 +17,7 @@ public final class PackageManager {
   }
 
   public PackageManager() {
-    Package commonLisp = new Package(Set.of("common-lisp", "cl"), Set.of());
-    COMMON_LISP_FUNCTIONS.forEach(commonLisp::addFunction);
-    COMMON_LISP_VARIABLES.forEach(commonLisp::addVariable);
+    Package commonLisp = new CommonLispPackage();
     add(commonLisp);
     Package commonLispUser = new Package(Set.of("common-lisp-user", "cl-user"), Set.of(commonLisp));
     add(commonLispUser);
