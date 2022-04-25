6179130cfe1795a23b1584784c2bc0153b4e8c9f
JeromeA <jerome.abela@gmail.com>
2022-04-25 10:04:33 +0100

    Simplify inplace refactoring code.

--- src/main/java/org/ax1/lisp/refactoring/LispRefactoringSupportProvider.java
+++ src/main/java/org/ax1/lisp/refactoring/LispRefactoringSupportProvider.java
@@ -7,20 +7,12 @@ import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
 public class LispRefactoringSupportProvider extends RefactoringSupportProvider {
-  @Override
-  public boolean isInplaceRenameAvailable(@NotNull PsiElement element, PsiElement context) {
-    if (element instanceof LispSymbol) {
-      LispSymbol symbol = (LispSymbol) element;
-      return symbol.isVariableDefinition();
-    }
-    return false;
-  }
 
   @Override
   public boolean isMemberInplaceRenameAvailable(@NotNull PsiElement element, @Nullable PsiElement context) {
     if (element instanceof LispSymbol) {
       LispSymbol symbol = (LispSymbol) element;
-      return symbol.isFunctionDefinition();
+      return symbol.isFunctionDefinition() || symbol.isVariableDefinition();
     }
     return false;
   }
