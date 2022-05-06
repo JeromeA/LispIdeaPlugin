1afdf86150930684bec58826d65890f6ca6cf4db
JeromeA <jerome.abela@gmail.com>
2022-05-06 22:54:50 +0200

    Try to accept more inline renaming.

--- src/main/java/org/ax1/lisp/refactoring/LispRefactoringSupportProvider.java
+++ src/main/java/org/ax1/lisp/refactoring/LispRefactoringSupportProvider.java
@@ -9,7 +9,16 @@ import org.jetbrains.annotations.Nullable;
 public class LispRefactoringSupportProvider extends RefactoringSupportProvider {
 
   @Override
+  public boolean isInplaceRenameAvailable(@NotNull PsiElement element, PsiElement context) {
+    return isLispDefinition(element);
+  }
+
+  @Override
   public boolean isMemberInplaceRenameAvailable(@NotNull PsiElement element, @Nullable PsiElement context) {
+    return isLispDefinition(element);
+  }
+
+  private boolean isLispDefinition(@NotNull PsiElement element) {
     if (element instanceof LispSymbol) {
       LispSymbol symbol = (LispSymbol) element;
       return symbol.isFunctionDefinition() || symbol.isVariableDefinition();
