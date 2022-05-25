99b63e3ebefe4d37c2cf35954caac4d0414879d7
JeromeA <jerome.abela@gmail.com>
2022-05-25 15:29:55 +0100

    Add support for defmethod lambda list variables.

--- src/main/java/org/ax1/lisp/analysis/form/AnalyzeDefmethod.java
+++ src/main/java/org/ax1/lisp/analysis/form/AnalyzeDefmethod.java
@@ -1,15 +1,27 @@
 package org.ax1.lisp.analysis.form;
 
+import org.ax1.lisp.analysis.LexicalBindingManager;
 import org.ax1.lisp.analysis.SyntaxAnalyzer;
+import org.ax1.lisp.analysis.symbol.Symbol;
 import org.ax1.lisp.psi.LispList;
 import org.ax1.lisp.psi.LispSexp;
 import org.ax1.lisp.psi.LispSymbol;
+import org.jetbrains.annotations.NotNull;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 import static com.intellij.openapi.editor.DefaultLanguageHighlighterColors.FUNCTION_DECLARATION;
 
 public class AnalyzeDefmethod implements FormAnalyzer {
+
+  private static final Set<Symbol> KEYWORDS =
+      Stream.of("&KEY", "&OPTIONAL", "&REST")
+          .map(Symbol::clSymbol)
+          .collect(Collectors.toSet());
 
   @Override
   public void analyze(SyntaxAnalyzer analyzer, LispList form) {
@@ -31,18 +43,52 @@ public class AnalyzeDefmethod implements FormAnalyzer {
     int arg = 2;
     // Skip method qualifiers.
     while (arg < list.size() && list.get(arg).getList() == null) arg++;
+
     if (arg == list.size()) {
       analyzer.annotations.highlightError(form, "Missing lambda list");
       return;
     }
-    analyzeLambdaList(analyzer, list.get(arg++));
-    while(arg < list.size()) {
-      analyzer.analyzeForm(list.get(arg));
-      arg++;
+    LispList lambdaList = list.get(arg).getList();
+    if (lambdaList == null) {
+      analyzer.annotations.highlightError(list.get(arg), "Lambda list expected");
+      return;
+    }
+
+    List<LispSymbol> variables = getVariables(analyzer, lambdaList);
+    try(LexicalBindingManager.LexicalScope lexicalScope = analyzer.lexicalBindings.defineLexicalVariables(form, variables)) {
+      analyzer.analyzeForms(list, arg + 1);
     }
   }
 
-  private void analyzeLambdaList(SyntaxAnalyzer analyzer, LispSexp lambdaList) {
-    // TODO
+  @NotNull
+  private static List<LispSymbol> getVariables(SyntaxAnalyzer analyzer, LispList lambdaList) {
+    List<LispSymbol> result = new ArrayList<>();
+    for (LispSexp lispSexp : lambdaList.getSexpList()) {
+      LispSymbol variable = getVariable(analyzer, lispSexp);
+      if (variable != null) result.add(variable);
+    }
+    return result;
+  }
+
+  private static LispSymbol getVariable(SyntaxAnalyzer analyzer, LispSexp sexp) {
+    LispSymbol parsedSymbol = sexp.getSymbol();
+    LispList list = sexp.getList();
+    if (list != null) {
+      List<LispSexp> specialized = list.getSexpList();
+      if (specialized.size() != 2 || specialized.get(0).getSymbol() == null) {
+        analyzer.annotations.highlightError(list, "var-specializer expected");
+        return null;
+      }
+      parsedSymbol = specialized.get(0).getSymbol();
+    }
+    if (parsedSymbol != null) {
+      Symbol symbol = analyzer.packageManager.getSymbol(parsedSymbol);
+      if (KEYWORDS.contains(symbol)) {
+        analyzer.annotations.highlightConstant(parsedSymbol);
+      } else {
+        return parsedSymbol;
+      }
+    }
+    return null;
   }
 }
