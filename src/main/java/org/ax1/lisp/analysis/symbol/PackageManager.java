537f47dc72426a87a8decccf704da1134474531b
JeromeA <jerome.abela@gmail.com>
2022-12-01 15:59:08 +0000

    Create default package when a symbol is requested

--- src/main/java/org/ax1/lisp/analysis/symbol/PackageManager.java
+++ src/main/java/org/ax1/lisp/analysis/symbol/PackageManager.java
@@ -69,21 +69,20 @@ public final class PackageManager {
     if (doubleColon > 0) {
       String packageName = name.substring(0, doubleColon);
       String symbolName = name.substring(doubleColon + 2);
-      LispPackage lispPackage = packages.get(packageName);
-      if (lispPackage == null) {
-        return null;
-      }
-      return lispPackage.findSymbol(symbolName);
+      LispPackage lispPackage = getOrCreatePackage(packageName);
+      // If we were Lisp, we would call findSymbol, which can return null. But we want to be able to manipulate
+      // that unknown symbol, find all its occurrences, etc, so we really want it to exist. If we need to mark it as
+      // invalid, this will have to be done at a later stage.
+      return lispPackage.intern(symbolName);
     }
     int colon = name.indexOf(":");
     if (colon > 0) {
       String packageName = name.substring(0, colon);
       String symbolName = name.substring(colon + 1);
-      LispPackage lispPackage = packages.get(packageName);
-      if (lispPackage == null) {
-        return null;
-      }
-      return lispPackage.findExportedSymbol(symbolName);
+      LispPackage lispPackage = getOrCreatePackage(packageName);
+      // If we were Lisp, we would call findExportedSymbol, which can return null. But we want to be able to manipulate
+      // that unknown symbol, so we really want it to exist.
+      return lispPackage.intern(symbolName);
     }
     return currentPackage.intern(name);
   }
