cb9fdc2f4a1ed0f2504e43be17b3b341126639a4
JeromeA <jerome.abela@gmail.com>
2023-01-21 09:26:32 +0000

    Add 13x13 toolwindow icon.

--- src/main/java/org/ax1/lisp/LispIcons.java
+++ src/main/java/org/ax1/lisp/LispIcons.java
@@ -6,6 +6,7 @@ import com.intellij.openapi.util.IconLoader;
 
 public class LispIcons {
   public static final Icon FILE = IconLoader.getIcon("/icons/lambda.svg", LispIcons.class);
+  public static final Icon RUN = IconLoader.getIcon("/icons/lambda13.svg", LispIcons.class);
   public static final Icon TO_GENERIC = IconLoader.getIcon("/icons/toGeneric.svg", LispIcons.class);
   public static final Icon TO_METHOD = IconLoader.getIcon("/icons/toMethod.svg", LispIcons.class);
 }
