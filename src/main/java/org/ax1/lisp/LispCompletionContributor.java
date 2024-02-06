e2fa2f9b85fb23f8e1f168e4f3b029285933b82f
JeromeA <jerome.abela@gmail.com>
2024-02-06 17:37:35 +0000

    Support both upper/lower case names in completions.

--- src/main/java/org/ax1/lisp/LispCompletionContributor.java
+++ src/main/java/org/ax1/lisp/LispCompletionContributor.java
@@ -2,24 +2,27 @@ package org.ax1.lisp;
 
 import com.intellij.codeInsight.completion.*;
 import com.intellij.codeInsight.lookup.LookupElementBuilder;
-import com.intellij.patterns.PlatformPatterns;
 import com.intellij.patterns.StandardPatterns;
 import com.intellij.psi.PsiElement;
 import com.intellij.util.ProcessingContext;
 import org.ax1.lisp.analysis.ProjectData;
+import org.ax1.lisp.psi.LispSexp;
 import org.ax1.lisp.psi.impl.LispStringDesignator;
 import org.jetbrains.annotations.NotNull;
 
+import java.util.Collection;
+
 import static com.intellij.codeInsight.completion.CompletionUtilCore.DUMMY_IDENTIFIER_TRIMMED;
-import static org.ax1.lisp.analysis.BaseLispElement.Type.FUNCTION_USAGE;
-import static org.ax1.lisp.analysis.BaseLispElement.Type.VARIABLE_USAGE;
+import static com.intellij.patterns.PlatformPatterns.psiElement;
 import static org.ax1.lisp.psi.LispTypes.STRING_CONTENT_TOKEN;
 import static org.ax1.lisp.psi.LispTypes.SYMBOL_TOKEN;
 
 public class LispCompletionContributor extends CompletionContributor {
 
   public LispCompletionContributor() {
-    extend(CompletionType.BASIC, StandardPatterns.or(PlatformPatterns.psiElement(SYMBOL_TOKEN), PlatformPatterns.psiElement(STRING_CONTENT_TOKEN)), new LispCompletionProvider());
+    extend(CompletionType.BASIC,
+        StandardPatterns.or(psiElement(SYMBOL_TOKEN), psiElement(STRING_CONTENT_TOKEN)),
+        new LispCompletionProvider());
   }
 
   private static class LispCompletionProvider extends CompletionProvider<CompletionParameters> {
@@ -27,27 +30,34 @@ public class LispCompletionContributor extends CompletionContributor {
     protected void addCompletions(@NotNull CompletionParameters parameters, @NotNull ProcessingContext context, @NotNull CompletionResultSet result) {
       PsiElement symbolToken = parameters.getPosition();
       String tokenText = symbolToken.getText();
-      result = result.withPrefixMatcher(tokenText.substring(0, tokenText.length() - DUMMY_IDENTIFIER_TRIMMED.length()));
-      // If we are data, no completion.
-      if (!(symbolToken instanceof LispStringDesignator)) {
-        return;
+      String prefix = tokenText.substring(0, tokenText.length() - DUMMY_IDENTIFIER_TRIMMED.length());
+      boolean isPrefixUpper = !prefix.isEmpty() && prefix.toUpperCase().equals(prefix);
+      result = result.withPrefixMatcher(prefix);
+      LispStringDesignator stringDesignator = getStringDesignator(symbolToken);
+      if (stringDesignator == null) return;
+      ProjectData projectData = ProjectData.getInstance(symbolToken.getProject());
+      Collection<String> names;
+      switch(stringDesignator.getType()) {
+        case FUNCTION_USAGE:
+          names = projectData.getAllFunctionDefinitionNames();
+          break;
+        case VARIABLE_USAGE:
+          names = projectData.getAllVariableDefinitionNames();
+          break;
+        default:
+          return;
       }
-      LispStringDesignator stringDesignator = (LispStringDesignator) symbolToken;
-      // If we are a function usage, complete with all known functions
-      if (stringDesignator.getType() == FUNCTION_USAGE) {
-        ProjectData.getInstance(symbolToken.getProject())
-            .getAllFunctionDefinitionNames()
-            .stream()
-            .map(LookupElementBuilder::create)
-            .forEach(result::addElement);
-      }
-      // If we are a variable usae, complete with all global variables
-      if (stringDesignator.getType() == VARIABLE_USAGE) {
-        ProjectData.getInstance(symbolToken.getProject())
-            .getAllVariableDefinitionNames()
-            .stream()
-            .map(LookupElementBuilder::create)
-            .forEach(result::addElement);
+      names.stream()
+          .map(n -> isPrefixUpper ? n : n.toLowerCase())
+          .map(LookupElementBuilder::create)
+          .forEach(result::addElement);
+    }
+
+    private LispStringDesignator getStringDesignator(PsiElement element) {
+      while (true) {
+        if (element instanceof LispStringDesignator) return (LispStringDesignator) element;
+        if (element instanceof LispSexp) return null;
+        element = element.getParent();
       }
     }
   }
